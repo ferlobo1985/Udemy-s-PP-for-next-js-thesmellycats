7e46cdaeeb9150c63b277a15f70b462d64495400
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-06-08 20:20:21 -0300

    51-Update email stepper part 2

--- components/users/account/emailstepper.js
+++ components/users/account/emailstepper.js
@@ -20,14 +20,15 @@ import { useDispatch, useSelector } from "react-redux";
 import { errorDispatcher } from 'store/actions/notifications.action';
 import { userSignOut } from 'store/actions/user.action'
 
+
 const EmailStepper = () => {
     const [ loading,setLoading] = useState(false);
     const [emailModal,setEmailModal] = useState(false)
     const dispatch = useDispatch();
     const router = useRouter();
 
-    const [active,setActiveStep] = useState(0);
-    const step = ['Enter old email','Enter new email','Are you sure ?'];
+    const [activeStep,setActiveStep] = useState(0);
+    const steps = ['Enter old email','Enter new email','Are you sure ?'];
     const user = useSelector(state=>state.user)
 
     const formik = useFormik({
@@ -65,6 +66,27 @@ const EmailStepper = () => {
       const openModal = () => setEmailModal(true);
       const closeModal = () => setEmailModal(false);
 
+      const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1)
+      }
+
+      const handleBack = () => {
+            setActiveStep((prevActiveStep) => prevActiveStep - 1)
+        }
+
+    const nextBtn = () => (
+        <Button className="mt-3" variant="container" color="primary" onClick={handleNext} >
+            Next
+        </Button>
+    )
+
+    const backsBtn = () => (
+        <Button className="mt-3 ml-2" variant="container" onClick={handleBack} >
+            Back
+        </Button>
+    )
+
+
 
     return(
         <>
@@ -88,11 +110,87 @@ const EmailStepper = () => {
                 >
                  Edit edit mail
                 </Button>
-
-
             </form>
 
+            <Modal size="lg" centered show={emailModal} onHide={closeModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Update your email</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
 
+                    <Stepper activeStep={activeStep}>
+                        { steps.map( (label,index) =>{
+                            return(
+                                <Step key={label}>
+                                    <StepLabel>{label}</StepLabel>
+                                </Step>
+                            )
+                        })}
+                    </Stepper>
+
+                    <form className="mt-3 stepper_form" onSubmit={formik.handleSubmit}>
+
+                        { activeStep === 0 ?
+                             <div className="form-group">
+                                <TextField
+                                    style={{ width: "100%" }}
+                                    name="email"
+                                    label="Enter your current email"
+                                    variant="outlined"
+                                    {...formik.getFieldProps("email")}
+                                    {...errorHelper(formik, "email")}
+                                />
+                                { formik.values.email && !formik.errors.email ?
+                                    nextBtn()
+                                   :null
+                                }
+                            </div>
+                        :null}
+
+                        { activeStep === 1 ?
+                            <div className="form-group">
+                                <TextField
+                                    style={{ width: "100%" }}
+                                    name="newemail"
+                                    label="Enter your new email"
+                                    variant="outlined"
+                                    {...formik.getFieldProps("newemail")}
+                                    {...errorHelper(formik, "newemail")}
+                                />
+                                { formik.values.newemail && !formik.errors.newemail ?
+                                    nextBtn()
+                                   :null
+                                }
+                                { backsBtn()}
+                            </div>
+                        :null}
+
+
+                        { activeStep === 2 ?
+                            <div className="form-group">
+                                { loading ?
+                                    <Loader/>
+                                :
+                                    <>
+                                    <Alert variant="warning">
+                                    Remember, you will be signed out and you need to sign in with your new email.
+                                    </Alert>
+                                    <Button 
+                                        className="mt-3"
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={formik.submitForm}
+                                    >   
+                                        Edit email
+                                    </Button>
+                                    {backsBtn()}
+                                    </>
+                                }
+                            </div>
+                        :null}
+                    </form>
+                </Modal.Body>
+            </Modal>
         </>
     )
 }
