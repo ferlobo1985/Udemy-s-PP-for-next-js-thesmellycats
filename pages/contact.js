334df368317fca093a88f5f0c50499fe98514113
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-06-08 18:14:18 -0300

    45-Creating the contact page

---
+++ pages/contact.js
@@ -0,0 +1,98 @@
+import { useState } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { errorHelper } from 'helpers/functions'
+
+import Loader from 'helpers/loader';
+import axios from 'axios';
+
+import { useDispatch } from 'react-redux'
+import { errorDispatcher, successDispatcher } from 'store/actions/notifications.action'
+
+
+const Contact = () => {
+    const [loading, setLoading] = useState(false);
+    const dispatch = useDispatch();
+
+    const formik = useFormik({
+        initialValues:{name:'',email:'',message:''},
+        validationSchema: Yup.object({
+            name:Yup.string()
+            .required('Sorry the name is required'),
+            email:Yup.string()
+            .required('Sorry the email is required')
+            .email('This is an invalid email'),
+            message:Yup.string()
+            .required('Sorry the message is required'),
+        }),
+        onSubmit:(values,{resetForm})=>{
+            //setLoading(true)
+            
+        
+        }
+    });
+
+
+    return(
+        <div className="container page_container">
+            <div className="contact_wrapper">
+
+                <form className="mt-3 article_form" onSubmit={formik.handleSubmit}>
+                <h1>Contact us</h1>
+
+                    <div className="form-group">
+                        <TextField
+                            name="name"
+                            label="Enter your name"
+                            variant="outlined"
+                            {...formik.getFieldProps('name')}
+                            {...errorHelper(formik,'name')}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <TextField
+                            name="email"
+                            label="Enter your email"
+                            variant="outlined"
+                            {...formik.getFieldProps('email')}
+                            {...errorHelper(formik,'email')}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <TextField
+                            name="email"
+                            label="Enter your message"
+                            multiline
+                            rows={4}
+                            variant="outlined"
+                            {...formik.getFieldProps('message')}
+                            {...errorHelper(formik,'message')}
+                        />
+                    </div>
+
+
+                    { loading ?
+                        <Loader/>
+                    :
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            type="submit"
+                        >
+                            Send message
+                        </Button>
+                    }
+
+                </form>
+
+            </div>
+        </div>
+    )
+}
+
+export default Contact;
